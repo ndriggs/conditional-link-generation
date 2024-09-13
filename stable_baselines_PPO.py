590ec72d2e306d13095b4e53241b8560072c865d
Nathaniel Driggs <nathanieldriggs@gmail.com>
2024-09-13 17:49:43 -0600

    attempting to train sb3

--- stable_baselines_PPO.py
+++ stable_baselines_PPO.py
@@ -2,20 +2,52 @@ import gymnasium as gym
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.base_class import BaseAlgorithm
+from stable_baselines3.common.evaluation import evaluate_policy
+import matplotlib.pyplot as plt
+
+from gymnasium.envs.registration import register
+
+register(
+    id='LinkBuilderEnv-v0',  
+    entry_point='envs.signature_environment:LinkBuilderEnv',  
+)
 
 # Parallel environments
-vec_env = make_vec_env("CartPole-v1", n_envs=4)
+env = gym.make("LinkBuilderEnv-v0")
 
-model = PPO("MlpPolicy", vec_env, verbose=1)
+model = PPO("MlpPolicy",env,verbose=1)
+
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100, warn=False)
+print(f"mean_reward before training: {mean_reward:.2f} +/- {std_reward:.2f}")
+
 model.learn(total_timesteps=25000)
-model.save("ppo_cartpole")
+model.save("ppo_link_builder")
 
-del model # remove to demonstrate saving and loading
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+print(f"mean_reward after training:{mean_reward:.2f} +/- {std_reward:.2f}")
 
-model = PPO.load("ppo_cartpole")
+# del model # remove to demonstrate saving and loading
 
-obs = vec_env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render("human")
+# model = PPO.load("ppo_link_builder")
+
+# rewards_per_episode = []
+# for episode in range(100) : 
+#     rewards_this_episode = []
+#     obs = env.reset()
+#     done = False 
+#     while not done : 
+#         action, _states = model.predict(obs)
+#         obs, reward, terminated, truncated, info = env.step(action)
+#         rewards_this_episode.append(reward)
+#         done = terminated or truncated
+#     rewards_per_episode.append(sum(rewards_this_episode))
+
+# print('mean:', np.mean(rewards_per_episode))
+# print('std:', np.std(rewards_per_episode))
+
+# plt.figure(figsize=(10, 6))
+# plt.plot(rewards_per_episode)
+# plt.ylabel('Reward per Episode')
+# plt.xlabel('Episode Number')
+# plt.savefig('cumulative_reward.png')
