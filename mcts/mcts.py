86e17b143e147f5a177f14f02b3b06219a1f04ec
Nathaniel Driggs <nathanieldriggs@gmail.com>
2024-10-21 18:15:30 -0600

    added MCTS resources

---
+++ mcts/mcts.py
@@ -0,0 +1,9 @@
+# https://towardsdatascience.com/from-scratch-implementation-of-alphazero-for-connect4-f73d4554002a
+
+# original paper https://www.nature.com/articles/nature24270
+# https://github.com/michaelnny/alpha_zero
+# https://github.com/tensorflow/minigo
+# https://github.com/suragnair/alpha-zero-general
+
+# applications of MCTS beyond games 
+# file:///C:/Users/natha/Downloads/s10489-023-05240-w.pdf
