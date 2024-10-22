43e3615d1d5bfb77d2f781835cd1b9a44cc91637
Nathaniel Driggs <nathanieldriggs@gmail.com>
2024-10-22 13:50:39 -0600

    added comments

--- predicting_signature/predict_signature.py
+++ predicting_signature/predict_signature.py
@@ -8,12 +8,19 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    # options: mlp, cnn, transformer_encoder, reformer, gnn
     parser.add_argument('--model', type=str, default='cnn')
+    # only applicable to mlp
     parser.add_argument('--hidden_size', type=int, default=100)
+    # only applicable to mlp
     parser.add_argument('--dropout', type=float, default=0.3)
+    # options: clip_then_normalize, abs_log_then_scale (only applicable to cnn and mlp)
+    # other option: remove_cancelations (only applicable to transformer_encoder, reformer, and gnn)
     parser.add_argument('--preprocessing', type=str, default=None)
     parser.add_argument('--accelerator', type=str, default='gpu')
+    # options: 2 or 3 (only applicable to cnn)
     parser.add_argument('--kernel_size', type=int, default=2)
+    # only applicable to cnn
     parser.add_argument('--layer_norm', type=bool, default=True)
     parser.add_argument('--regression', type=bool, default=True)
     return parser.parse_args()
