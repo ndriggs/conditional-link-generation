7c9d7e7cbe41112224e07469b1e59eed827e40bf
Nathaniel Driggs <nathanieldriggs@gmail.com>
2024-11-25 12:37:10 -0700

    registering new SigDetEnv

--- src/link_generation/envs/__init__.py
+++ src/link_generation/envs/__init__.py
@@ -4,3 +4,8 @@ register(
     id='SignatureEnv-v0',  
     entry_point='link_generation.envs.signature_env:SignatureEnv', 
 )
+
+register(
+    id='SigDetEnv-v0',  
+    entry_point='link_generation.envs.sig_det_env:SigDetEnv', 
+)
